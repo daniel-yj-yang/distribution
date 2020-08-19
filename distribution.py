f1bc94929a63216a5b844cbd05a8f55efab090e1
Daniel Yang <daniel.yj.yang@gmail.com>
2020-08-19 02:50:35 -0500

    Update

--- distribution.py
+++ distribution.py
@@ -73,8 +73,8 @@ from numpy import arange
 from matplotlib import pyplot
 from scipy.stats import chi2
 # define the distribution parameters
-sample_space = arange(0, 50, 0.01)
-dof = 20
+sample_space = arange(0, 10, 0.01)
+dof = 1 # 3
 # calculate the pdf
 pdf = chi2.pdf(sample_space, dof)
 # plot
