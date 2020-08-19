1d77bcf21522b56cc6e398c3ffe405dd9ff511db
Daniel Yang <daniel.yj.yang@gmail.com>
2020-08-19 03:07:07 -0500

    Update

--- distribution.py
+++ distribution.py
@@ -85,3 +85,17 @@ cdf = chi2.cdf(sample_space, dof)
 # plot
 pyplot.plot(sample_space, cdf)
 pyplot.show()
+
+
+
+# Distribution of swish function
+# https://en.wikipedia.org/wiki/Swish_function
+from numpy import arange
+def swish(x, beta):
+    return x/(1+exp(-beta*x))
+
+x_sample_space = arange(-5, 5.01, 0.01)
+beta = 1 # 0.5
+y = swish(x_sample_space, beta)
+plt.plot(x_sample_space, y)
+plt.show()
